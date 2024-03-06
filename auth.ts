f3f159b226b32e7ef68b7e6679f75195022860bc
Siem Debesay <siem3500@gmail.com>
2024-03-06 16:56:47 -0700

    added a callback function and added user roles to db schema

--- auth.ts
+++ auth.ts
@@ -1,8 +1,17 @@
-import NextAuth from "next-auth"
+import NextAuth, { DefaultSession } from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 
 import { db } from "./lib/db"
 import authConfig from "./auth.config"
+import { getUserById } from "./data/user"
+
+declare module "@auth/core"{
+  interface Session {
+    user: {
+      
+    } & DefaultSession["user"]
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -10,6 +19,29 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session ({ session, token}) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt ({ token }) {
+      if (!token.sub) return token;
+      // if there is a token, we want to get the user by the token.sub
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+      // if there is a user, we want to assign user role to the token
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt"},
     ...authConfig,
