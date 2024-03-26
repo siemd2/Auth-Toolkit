6378f5f453ae138cea9238d2bc7f9a1a6b1cb81a
Siem Debesay <siem3500@gmail.com>
2024-03-25 22:50:00 -0600

    email verification server action finished

---
+++ actions/new-verification.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
+import { getVerficicationTokenByToken } from "@/data/verification-token";
+
+export const newVerification = async (token: string) => {
+    const existingToken = await getVerficicationTokenByToken(token);
+
+    if (!existingToken) {
+        return { error: "Token does not exist!"};
+    }
+
+    const hasExpired = new Date(existingToken.expires) < new Date();
+
+    if (hasExpired) {
+        return { error: "Token has expired!"};
+    }
+
+    const existingUser = await getUserByEmail(existingToken.email);
+
+    if (!existingUser) {
+        return { error: "Email does not exist!"};
+    }  
+    
+    await db.user.update({
+        where: { id: existingUser.id },
+        data: {
+            emailVerified: new Date(),
+            email: existingToken.email
+        }
+    });
+
+    await db.verificationToken.delete({
+        where: { id: existingToken.id }
+    });
+
+    return { success: "Email Verified!" };
+}
